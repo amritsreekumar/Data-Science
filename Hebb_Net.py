857756b45ff15e88d9b6bee14be69ab4cbe32cd6
Amrit Sreekumar <amrit.sreekumar@gmail.com>
2018-11-08 19:23:55 +0530

    Inputs added

---
+++ Hebb_Net.py
@@ -0,0 +1,14 @@
+x = []
+y = []
+print(" Enter the number of inputs")
+n = int(input())
+print("Enter the number of input vectors")
+n1 = int(input())
+for i in range(0,n):
+	for j in range(0,n1):
+		print ("Enter input number: " + str(j+1))
+		x.append(int(input()))
+	bias = 1
+	print("Enter the output")
+	y.append(int(input))
+    
