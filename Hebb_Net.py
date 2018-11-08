c6e8c33750138c7b68da62a16d2fd2b43ffe2f52
Amrit Sreekumar <amrit.sreekumar@gmail.com>
2018-11-08 22:13:48 +0530

    Added Bias

--- Hebb_Net.py
+++ Hebb_Net.py
@@ -1,11 +1,10 @@
 x = []
-bias = []
+bias = 0
 print(" Enter the number of inputs")
 n = int(input())
 print("Enter the number of input vectors")
 n1 = int(input())
 w = [0] * n1
-bias = [0] * n
 for i in range(0,n):
 	for j in range(0,n1):
 		print ("Enter input number: " + str(j+1))
@@ -15,7 +14,8 @@ for i in range(0,n):
 	for k in range(0,n1):
 		dw = x[k]*y
 		w[k] = w[k] + dw
-	
+	db = y
+	bias = bias + db
 
 
     
