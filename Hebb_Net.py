8d0b286267666164c94ca2489728dc792953a121
Amrit Sreekumar <amrit.sreekumar@gmail.com>
2018-11-13 21:08:24 +0530

    Final changes made

--- Hebb_Net.py
+++ Hebb_Net.py
@@ -9,7 +9,7 @@ n1 = int(input())
 w = [0] * n1
 for i in range(0,n):
 	for j in range(0,n1):
-		print ("Enter input number: " + str(j+1))
+		print ("Enter input number " + str(j+1) + " of " + str(i+1))
 		x.insert(j, int(input()))
 	print("Enter the output")
 	y = int(input())
